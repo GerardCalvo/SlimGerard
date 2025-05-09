495fc051496802df5bc778e38540177b6c19bf5d
GerardCalvo <gerard.calvo.7e8@itb.cat>
2025-05-09 07:45:24 +0000

    commit final

--- public/db/database.proc.php
+++ public/db/database.proc.php
@@ -13,7 +13,8 @@ $db->exec("CREATE TABLE IF NOT EXISTS 'musics' (
 $db->exec("INSERT INTO 'musics' ('mus_nom', 'mus_naixement', 'mus_estil', 'mus_imatge') VALUES 
 ('Ludwig van Beethoven', '1770-12-17', 'Clàssic', 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Beethoven.jpg'),
 ('Freddie Mercury', '1946-09-05', 'Rock', 'https://hips.hearstapps.com/hmg-prod/images/freddie-mercury-queen-chaqueta-amarilla-9-agosto-1986-1502982487.jpg'),
-('Michael Jackson', '1958-08-29', 'Pop', 'https://media.vogue.es/photos/6538fee5e68177b3d0971e69/1:1/w_640,h_640,c_limit/Michael%20Jackson.jpeg')
+('Michael Jackson', '1958-08-29', 'Pop', 'https://media.vogue.es/photos/6538fee5e68177b3d0971e69/1:1/w_640,h_640,c_limit/Michael%20Jackson.jpeg'),
+('Amador Rivas', '1976-12-01', 'Pop', 'https://estaticos-cdn.prensaiberica.es/clip/4a0e8bed-44a7-4de8-a6cb-0ffbfcd0255d_alta-libre-aspect-ratio_default_0.jpg')
 ;");
 
 $db->close();
